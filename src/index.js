76327257f17b281f1612ef65cc38679429cde1e8
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-08-10 23:25:04 +0200

    list, info done

--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ const DEBOUNCE_DELAY = 300;
 const input = document.querySelector('input#search-box');
 const countryList = document.querySelector('ul.country-list');
 const countryInfo = document.querySelector('div.country-info');
+const body = document.querySelector("body");
+
+body.style.padding = "15px";
+body.style.backgroundColor = "#f2f5f2";
+
+input.style.width = "30vw";
+input.style.fontWeight = "600";
 
 
 countryList.style.listStyle = "none";
@@ -15,16 +22,23 @@ countryList.style.margin = "0";
 countryList.style.padding = "0";
 
 const renderInfo = (countries) => {
-  let markupList = countries
+  let markupInfo = countries
     .map(country => {
-      return ``;
-  })
+      return `<p class="info-header" style="display: flex; align-items: center; font-size: 40px; font-weight: 700; margin: 0; margin-bottom: 30px;" ><img src="${country.flags.svg}" width="30" style="display: inline-block" /><span class="country-span"
+      style="padding-left: 5px;">${country.name.official}</span></p> <ul class="info-list" style="list-style: none; padding: 0; margin: 0 " >
+        <li class="info-element" style="margin-bottom: 18px;"><span class="info-span" style="font-size: 20px; font-weight: 700; ">Capital:</span><span class="country-info-span" style="font-size: 20px; margin-left: 5px; font-weight: 600 ">${country.capital}</span></li>
+        <li class="info-element" style="margin-bottom: 18px;"><span class="info-span" style="font-size: 20px; font-weight: 700; ">Population:</span><span class="country-info-span" style="font-size: 20px; margin-left: 5px; font-weight: 600">${country.population}</span></li>
+        <li class="info-element" style="margin-bottom: 18px;"><span class="info-span" style="font-size: 20px; font-weight: 700; ">Languages:</span><span class="country-info-span" style="font-size: 20px; margin-left: 5px; font-weight: 600">${Object.values(country.languages).join(', ')}</span></li>
+      </ul> `;
+    })
+    .join("");
+  countryInfo.innerHTML = markupInfo;
 };
 
 const renderList = (countries) => {
   let markupList = countries
     .map(country => {
-      return `<li class="country-element" style="display: flex; align-items: center; padding-bottom: 5px"><img src="${country.flags.svg}" width="30" style="display: inline-block" /><span class="country-span" style="padding-left: 5px;">${country.name.official}</span></li>`
+      return `<li class="country-element" style="display: flex; align-items: center; padding-bottom: 5px"><img src="${country.flags.svg}" width="30" style="display: inline-block" /><span class="country-span" style="padding-left: 5px; font-weight: 600">${country.name.official}</span></li>`
     })
     .join("");
   countryList.innerHTML = markupList;
@@ -42,7 +56,7 @@ input.addEventListener('input', debounce((event) => {
   return fetchCountries(trimInput)
     .then(countries => {
       if (countries.length > 10) {
-        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.", {width: '50vw'});
+        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.", {width: '35vw'});
         countryList.innerHTML = "";
     countryInfo.innerHTML = "";
       };
@@ -50,13 +64,17 @@ input.addEventListener('input', debounce((event) => {
         countryInfo.innerHTML = "";
         renderList(countries);
       };
+      if (countries.length === 1) {
+        countryList.innerHTML = "";
+        renderInfo(countries);
+      };
   console.log(countries);
   console.log(countries[0].flags.svg);
   console.log(countries[0].name.official);
   console.log(countries[0].capital[0]);
   console.log(countries[0].population);
   let object = countries[0].languages;
-  console.log(Object.values(object).join(', '));
+  console.log(Object.values(countries[0].languages).join(', '));
   })
   .catch(error => {})
   console.log(trimInput);
