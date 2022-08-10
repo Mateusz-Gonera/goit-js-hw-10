7d27747faef91ea61557aeb36b2ebc46c7a6d2a9
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-08-10 20:48:10 +0200

    list done

--- src/index.js
+++ src/index.js
@@ -9,11 +9,17 @@ const input = document.querySelector('input#search-box');
 const countryList = document.querySelector('ul.country-list');
 const countryInfo = document.querySelector('div.country-info');
 
+
 countryList.style.listStyle = "none";
 countryList.style.margin = "0";
 countryList.style.padding = "0";
 
-
+const renderInfo = (countries) => {
+  let markupList = countries
+    .map(country => {
+      return ``;
+  })
+};
 
 const renderList = (countries) => {
   let markupList = countries
@@ -24,15 +30,24 @@ const renderList = (countries) => {
   countryList.innerHTML = markupList;
 };
 
+
+
 input.addEventListener('input', debounce((event) => {
   let trimInput = input.value.trim();
-  if (trimInput === "") return;
+  if (trimInput === "") {
+    countryList.innerHTML = "";
+    countryInfo.innerHTML = "";
+    return
+  };
   return fetchCountries(trimInput)
     .then(countries => {
       if (countries.length > 10) {
-        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.", {width: '50vw'});
+        countryList.innerHTML = "";
+    countryInfo.innerHTML = "";
       };
       if (countries.length <= 10 && countries.length >= 2) {
+        countryInfo.innerHTML = "";
         renderList(countries);
       };
   console.log(countries);
