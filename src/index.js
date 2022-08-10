a7f714cfdbecfd61b5ea4d3bd410bfb6ce1ac041
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-08-10 12:45:34 +0200

    sprawdzenie url countries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+fetchCountries('sweden').then(countries => {
+  console.log(countries[0].flags.svg);
+  console.log(countries[0].name.official);
+  console.log(countries[0].capital[0]);
+  console.log(countries[0].population);
+  let object = countries[0].languages;
+  console.log(Object.values(object).join(', '));
+});
