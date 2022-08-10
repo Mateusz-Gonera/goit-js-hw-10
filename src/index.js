a86e9bd5dea229d2a6abdc1af213508dec69a3ad
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-08-10 13:03:00 +0200

    import bibliotek

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+
+const input = document.querySelector('input#search-box');
+
+input.addEventListener('input', debounce((event) => {
+  console.log(input.value);
+}, 500))
 
 fetchCountries('sweden').then(countries => {
   console.log(countries[0].flags.svg);
