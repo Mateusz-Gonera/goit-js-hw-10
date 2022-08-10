209a95c0b1c9bb2f689ce9660206beff2862954c
Mateusz-Gonera <mateuszg1992@wp.pl>
2022-08-10 17:22:01 +0200

    lista początek

--- src/index.js
+++ src/index.js
@@ -9,14 +9,32 @@ const input = document.querySelector('input#search-box');
 const countryList = document.querySelector('ul.country-list');
 const countryInfo = document.querySelector('div.country-info');
 
+countryList.style.listStyle = "none";
+countryList.style.margin = "0";
+countryList.style.padding = "0";
+
+
+
+const renderList = (countries) => {
+  let markupList = countries
+    .map(country => {
+      return `<li class="country-element" style="display: flex; align-items: center; padding-bottom: 5px"><img src="${country.flags.svg}" width="30" style="display: inline-block" /><span class="country-span" style="padding-left: 5px;">${country.name.official}</span></li>`
+    })
+    .join("");
+  countryList.innerHTML = markupList;
+};
 
 input.addEventListener('input', debounce((event) => {
   let trimInput = input.value.trim();
-  console.log(trimInput);
-}, DEBOUNCE_DELAY))
-
-fetchCountries('peru')
-  .then(countries => {
+  if (trimInput === "") return;
+  return fetchCountries(trimInput)
+    .then(countries => {
+      if (countries.length > 10) {
+        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+      };
+      if (countries.length <= 10 && countries.length >= 2) {
+        renderList(countries);
+      };
   console.log(countries);
   console.log(countries[0].flags.svg);
   console.log(countries[0].name.official);
@@ -26,3 +44,8 @@ fetchCountries('peru')
   console.log(Object.values(object).join(', '));
   })
   .catch(error => {})
+  console.log(trimInput);
+}, DEBOUNCE_DELAY))
+
+fetchCountries('peru')
+  
